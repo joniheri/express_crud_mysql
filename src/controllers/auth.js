0d379ff011a45390329b14127d99cb3f27ef9e0f
Jon <jonheri001@gmail.com>
2021-06-16 03:53:40 +0700

    encryption password with bcrypt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,12 @@
 const { user } = require("../../models");
 const joi = require("joi");
+const bcrypt = require("bcrypt");
 
 // Register
 exports.register = async (req, res) => {
   try {
     const data = req.body;
-    const { email } = req.body;
+    const { email, password } = req.body;
 
     const schema = joi.object({
       username: joi.string().min(6).required(),
@@ -34,15 +35,32 @@ exports.register = async (req, res) => {
     if (findEmail) {
       return res.send({
         status: "Failed",
-        message: `Rgister with email : ${email} already`,
+        message: `Email: ${email} already registered`,
         dataFindEmail: findEmail,
       });
     }
     // end check "email user" is exist
 
+    // bcrypt password
+    const hashStrenght = 10;
+    const hashedPassword = await bcrypt.hash(password, hashStrenght);
+    // end bcrypt password
+
+    // imput data to database
+    const dataUser = await user.create({
+      ...data,
+      password: hashedPassword,
+    });
+    //  end imput data to database
+
     res.send({
       status: "Respon Success",
       message: "Validate input ok",
+      data: {
+        email: dataUser.email,
+        username: dataUser.username,
+        password: dataUser.password,
+      },
     });
   } catch (error) {
     console.log(error);
